7b7bd117b4cd99de33a71000a36bed717bbbbb1f
pon08Oa <87184564+pon08Oa@users.noreply.github.com>
2022-11-24 19:59:50 +0900

    editor

--- js/editor.js
+++ js/editor.js
@@ -1,3 +1,12 @@
 const editor = new EditorJS({
     holder: 'editorjs',
 });
+
+const pre = document.getElementById("data");
+document.getElementById("save").addEventListener("click",()=>{
+    editor.save().then((outputData) => {
+        pre.textContent = JSON.stringify(outputData, null , "¥t");
+    }).catch((error) => {
+        console.log('Saving failed: ', error)
+    });
+})
