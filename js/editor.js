d3c855479c52640ec859ba56b6147c2aaf90641e
pon08Oa <87184564+pon08Oa@users.noreply.github.com>
2022-11-24 20:33:11 +0900

    空白バグ修正2

--- js/editor.js
+++ js/editor.js
@@ -5,7 +5,7 @@ const editor = new EditorJS({
 const pre = document.getElementById("data");
 document.getElementById("save").addEventListener("click",()=>{
     editor.save().then((outputData) => {
-        pre.textContent = JSON.stringify(outputData, null , "&nbsp");
+        pre.textContent = JSON.stringify(outputData, null , "  ");
     }).catch((error) => {
         console.log('Saving failed: ', error)
     });
